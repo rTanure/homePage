2f59309166a314d507931a9b8809b54bf450ff40
Richardy Tanure <richardy.tanure@hotmail.com>
2022-05-15 17:58:36 -0300

    vercel config

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 8080
 app.set('view engine', 'ejs')
 app.use(express.static(__dirname + '/public'))
 
-app.get('', (req, res)=>{
+app.get('/', (req, res)=>{
     res.render('pages/home')
 })
 
