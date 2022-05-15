84de1df5e7dea2cedab49efcf444856184067990
Richardy Tanure <richardy.tanure@hotmail.com>
2022-05-15 18:01:07 -0300

    fixing bug

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const PORT = process.env.PORT || 8080
 app.set('view engine', 'ejs')
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', (req, res)=>{
-    res.render('pages/home')
-})
+// app.get('/', (req, res)=>{
+//     res.render('pages/home')
+// })
 
 app.listen(PORT, ()=>{
     console.log(`Server running in port ${PORT}`)
